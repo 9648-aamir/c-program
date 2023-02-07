e864deaae5ec5d7472bbc2f53fdbcafe40bdd8b9
9648-aamir <aamir9648770208@gmail.com>
2023-02-07 12:34:05 +0530

    area of traigle

---
+++ areaoftraingle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b,h,a;
+    cout<<"Enter the height and base =";
+    cin>>b>>h;
+    
+    a=(b*h)/2;
+    
+    cout<<"Area is ="<<a;
+}
+
